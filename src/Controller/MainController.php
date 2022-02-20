cf7981364b3827b3700995e1d341452783984d6d
MehdiLarek <mehdi.larek2021@campus-eni.fr>
2022-02-20 01:28:03 +0100

    commit passe pas

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -17,7 +17,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
-//
+// //
 
 /**
  * @Route("/", name="main_")
